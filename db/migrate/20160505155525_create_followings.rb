2817193fe0583d4c83d60b02a7b9ad570840b0b8
Carlos Gonzalez & Kelson Adams <pair+cyberpolin.kelsonic@devbootcamp.com>
2016-05-06 11:46:51 -0700

    Edit user validations to not include regex for first_name, last_name, etc.

--- db/migrate/20160505155525_create_followings.rb
+++ db/migrate/20160505155525_create_followings.rb
@@ -2,6 +2,7 @@ class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |t|
       t.integer :follower_id
+      # t.integer :user_id
 
       t.timestamps null: false
     end
