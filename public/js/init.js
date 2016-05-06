7b055e285882ef1600aacff8735b2465f56ad6cf
Carlos Gonzalez & Kelson Adams <pair+cyberpolin.kelsonic@devbootcamp.com>
2016-05-06 14:51:48 -0700

    Add styles

--- public/js/init.js
+++ public/js/init.js
@@ -2,4 +2,5 @@ $(document).ready(func() {
   Materialize.updateTextFields();
   $('select').material_select();
   $('input#input_text, textarea#textarea1').characterCounter();
+  $(".button-collapse").sideNav();
 });
